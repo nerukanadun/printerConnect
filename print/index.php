7caefe597a59ca25f98ef369ddfdacf0f40460d1
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-12-22 22:40:26 +0530

    Update index.php

--- print/index.php
+++ print/index.php
@@ -5,11 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
 	</head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="https://sourcecodester.com">Sourcecodester</a>
-		</div>
-	</nav>
+	
 	<div class="col-md-3"></div>
 	<div class="col-md-6 well">
 		<h3 class="text-primary">PHP - How To Print A Document</h3>
